66fc05d21333f3ea31350f968523cbb512ab1e26
Kevin Leng <kevin.leng@dynatrace.com>
2020-07-13 15:19:59 +1000

    Update cartstest.sh

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -1,4 +1,4 @@
-export CARTS_IP=$(kubectl describe svc carts -n dev | grep 'LoadBalancer Ingress:' | sed 's~LoadBalancer Ingress:[ \t]*~~')
+export CARTS_IP=$1
 #export CARTS_URL="http:\/\/"$CARTS_IP":8080\/cart"
 
 sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_IP/g" carts_load1.jmx
