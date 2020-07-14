71ea46a49dba809d95dd7738e7887cce8057c4cb
Kevin Leng <kevin.leng@dynatrace.com>
2020-07-14 14:32:31 +1000

    Update installKeptn.sh

--- Keptn/installKeptn.sh
+++ Keptn/installKeptn.sh
@@ -3,13 +3,15 @@
 FUNCTIONS_FILE_REPO="https://raw.githubusercontent.com/KevLeng/keptn-in-a-box/no-keptn-install/functions.sh"
 FUNCTIONS_FILE='functions.sh'
 DOMAIN=
-keptn_install=true
-keptn_install_qualitygates=false
+
 
 curl -o functions.sh $FUNCTIONS_FILE_REPO
 
 # --- Loading the functions in the current shell
 source $FUNCTIONS_FILE
+
+keptn_install=true
+keptn_install_qualitygates=false
 
 setupMagicDomainPublicIp
 keptnInstall
