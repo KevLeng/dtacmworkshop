550439ad7540c19c3205e1df5ae372415b98d56f
Kevin Leng <kevin.leng@dynatrace.com>
2020-07-14 14:22:12 +1000

    Update installKeptn.sh

---
+++ Keptn/installKeptn.sh
@@ -0,0 +1,8 @@
+
+FUNCTIONS_FILE_REPO="https://raw.githubusercontent.com/KevLeng/keptn-in-a-box/no-keptn-install/functions.sh"
+
+# --- Loading the functions in the current shell
+source $FUNCTIONS_FILE
+
+
+keptnInstallClient
