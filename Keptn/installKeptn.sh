bad00e0001c39d3ab6fce7b624b9aac5d83f79e5
Kevin Leng <kevin.leng@dynatrace.com>
2020-07-14 14:45:27 +1000

    Update installKeptn.sh

--- Keptn/installKeptn.sh
+++ Keptn/installKeptn.sh
@@ -4,7 +4,6 @@ FUNCTIONS_FILE_REPO="https://raw.githubusercontent.com/KevLeng/keptn-in-a-box/no
 FUNCTIONS_FILE='functions.sh'
 DOMAIN=
 
-
 curl -o functions.sh $FUNCTIONS_FILE_REPO
 
 # --- Loading the functions in the current shell
@@ -15,5 +14,13 @@ keptn_install_qualitygates=false
 
 setupMagicDomainPublicIp
 keptnInstall
+
+KEPTN_API_TOKEN=$(kubectl get secret keptn-api-token -n keptn -ojsonpath={.data.keptn-api-token} | base64 --decode)
+KEPTN_ENDPOINT="https://api.keptn.$DOMAIN"
+KEPTN_BRIDGE="http://bridge.keptn.$DOMAIN"
+
+printInfo "KEPTN_BRIDGE: ${KEPTN_BRIDGE}"
+printInfo "KEPTN_ENDPOINT: ${KEPTN_ENDPOINT}"
+printInfo "KEPTN_API_TOKEN: ${KEPTN_API_TOKEN}"
 
 rm $FUNCTIONS_FILE
